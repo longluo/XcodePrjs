50c72061a63935a3a733358edf59509c317332ce
longluo <calmbody@163.com>
2013-04-06 01:26:16 +0800

    MyLocationDemo

---
+++ MyLocationDemo/MyLocationDemo/MyLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyLocationViewController.h
+//  MyLocationDemo
+//
+//  Created by Luo Long on 13-4-6.
+//  Copyright (c) 2013年 Long Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyLocationViewController : UIViewController
+
+@end
---
+++ MyLocationDemo/MyLocationDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MyLocationDemo
+//
+//  Created by Luo Long on 13-4-6.
+//  Copyright (c) 2013年 Long Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MyLocationAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MyLocationAppDelegate class]));
+    }
+}
