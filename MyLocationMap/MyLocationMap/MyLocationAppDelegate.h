cd0a5d0af5cc9ab731001dd741df0ae41ce7212f
longluo <calmbody@163.com>
2013-04-06 21:53:10 +0800

    Update MyLocationMap Project

---
+++ MyLocationMap/MyLocationMap/MyLocationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MyLocationAppDelegate.h
+//  MyLocationMap
+//
+//  Created by Luo Long on 13-4-6.
+//  Copyright (c) 2013年 Long Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyLocationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MyLocationMap/MyLocationMap/MyLocationViewController.m
@@ -0,0 +1,29 @@
+//
+//  MyLocationViewController.m
+//  MyLocationMap
+//
+//  Created by Luo Long on 13-4-6.
+//  Copyright (c) 2013年 Long Luo. All rights reserved.
+//
+
+#import "MyLocationViewController.h"
+
+@interface MyLocationViewController ()
+
+@end
+
+@implementation MyLocationViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
