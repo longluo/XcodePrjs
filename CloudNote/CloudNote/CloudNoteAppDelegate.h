8c1a4d936b5e5cfc8fd5148afe36f1efecd7b0cd
longluo <calmbody@163.com>
2013-04-08 00:52:07 +0800

    Update my iCloud iOS project.

---
+++ CloudNote/CloudNote/CloudNoteAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CloudNoteAppDelegate.h
+//  CloudNote
+//
+//  Created by Luo Long on 13-4-7.
+//  Copyright (c) 2013年 Long Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CloudNoteAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CloudNote/CloudNote/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CloudNote
+//
+//  Created by Luo Long on 13-4-7.
+//  Copyright (c) 2013年 Long Luo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CloudNoteAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CloudNoteAppDelegate class]));
+    }
+}
