3859035a2daea8b14d5f8053c8cca194a715827e
longluo <calmbody@163.com>
2013-05-29 00:08:13 +0800

    Update the Cocos2D-iPhone Game and a simple Game

---
+++ Cocos2DSimpleGame/Cocos2DSimpleGame/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Cocos2DSimpleGame
+//
+//  Created by Luo Long on 13-5-29.
+//  Copyright Long Luo 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
